3738dd9f23a8c7c8e3614f94565e290eb69c25f7
nutter123 <972360566@qq.com>
2017-07-08 15:14:31 +0800

    node

---
+++ nodeschool/learnyounode/mymodule.js
@@ -0,0 +1,13 @@
+var fs = require('fs')
+var path = require('path')
+
+module.exports = function(folder,ext,callback){
+  fs.readdir(folder,function(err,files){
+    if(err)
+      return callback(err)
+    files=rfiles.filter(function(file){
+      return path.extname(file)==='.'+ext
+    })
+    callback(null,files)
+  })
+}
